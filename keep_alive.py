760c96d2707166ce8df882bf7e5600c349db79f9
linavic <146561811+linavic@users.noreply.github.com>
2026-01-30 07:35:19 -0800

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -2,10 +2,11 @@ from aiohttp import web
 import os
 
 async def handle(request):
-    return web.Response(text="Bot is Alive!")
+    return web.Response(text="Bot is Alive and Running!")
 
 def start_server():
     app = web.Application()
     app.router.add_get('/', handle)
+    # Render מספק את הפורט במשתנה סביבה בשם PORT
     port = int(os.environ.get("PORT", 8080))
     return app, port
